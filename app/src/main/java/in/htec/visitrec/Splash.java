c36e4131c8f223a4878296f8edd598322f682a06
shiveshnavin <shiveshnavin@gmail.com>
2017-11-10 13:08:51 +0530

    Added Working Dropown with background selector

--- app/src/main/java/in/htec/visitrec/Splash.java
+++ app/src/main/java/in/htec/visitrec/Splash.java
@@ -1,13 +1,82 @@
 package in.htec.visitrec;
 
+import android.app.Activity;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+
+import in.htec.visitrec.adapters.SpinAdapter;
+import in.htec.visitrec.database.House;
 
 public class Splash extends AppCompatActivity {
 
+    public Context ctx;
+    public Activity act;
+
+    Spinner houseno;
+
+    ArrayList<House> houses;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ctx=this;
+        act=this;
         setContentView(R.layout.activity_splash);
+
+        houseno =(Spinner)findViewById(R.id.houseno);
+
+        houses=new ArrayList<>();
+
+        for(int i=0;i<10;i++)
+        {
+            houses.add(new House(i,"House No. "+i));
+        }
+
+        setUpUGS(houses);
+
+
+
+
     }
+
+
+
+    public void setUpUGS(final ArrayList<House> grps)
+    {
+
+        SpinAdapter adap=new SpinAdapter(ctx,android.R.layout.simple_spinner_item, grps );
+        adap.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        houseno.setAdapter(adap);
+        houseno.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view,
+                                       final  int position, long id) {
+
+
+
+
+
+
+
+
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapter) {  }
+        });
+
+
+    }
+
+
+
+
+
+
 }
---
+++ app/src/main/java/in/htec/visitrec/database/House.java
@@ -0,0 +1,31 @@
+package in.htec.visitrec.database;
+
+/**
+ * Created by shivesh on 10/11/17.
+ */
+
+public class House {
+
+
+
+    public int id=1;
+    public String name="Houses Name";
+    public String owner="Houses Name";
+    public String addr="Houses Name";
+    public String passwd="Houses Name";
+
+
+
+    public House()
+    {}
+
+    public House(int n,String na)
+    {
+
+        id=n;
+        name=na;
+
+    }
+
+
+}
