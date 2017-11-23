3154fe5ce799834c271e4a21318366aa8a3f280f
shiveshnavin <shiveshnavin@gmail.com>
2017-11-23 21:22:57 +0530

    Added Generic RecyclerView

--- app/src/main/java/in/htec/visitrec/Home.java
+++ app/src/main/java/in/htec/visitrec/Home.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
@@ -16,12 +17,16 @@ public class Home extends AppCompatActivity {
     DateTimePicker dt;
 
 
+    RecyclerView rv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        rv=(RecyclerView)findViewById(R.id.rec);
+
 
 
 
